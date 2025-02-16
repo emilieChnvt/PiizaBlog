2c04999789eb0013919a52ce8804b73c13cfb704
Emilie <emilie.chnvt@gmail.com>
2025-02-16 20:35:44 +0100

    Pizza.php entity added

---
+++ src/Entity/Pizza.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use Core\Attributes\Table;
+
+#[Table(name: 'pizzas')]
+class Pizza
+{
+    private int $id;
+    private string $name;
+    private string $description;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+}
