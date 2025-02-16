89a439cf2542c0e28bfdc424dcc080ac2c4ad85e
Emilie <emilie.chnvt@gmail.com>
2025-02-16 20:28:24 +0100

    start with docker

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Core\Controller\Controller;
+
+class HomeController extends Controller
+{
+
+    public function index()
+    {
+       return $this->render('home/documentation', []);
+    }
+}
